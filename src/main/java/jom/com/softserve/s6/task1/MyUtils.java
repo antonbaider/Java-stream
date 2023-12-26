e255ffa76d51bd5f405d91755e7b1c88fe73344b
antonbaider <email.b.anton@gmail.com>
2023-12-26 12:41:25 +0100

    done

--- src/main/java/jom/com/softserve/s6/task1/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task1/MyUtils.java
@@ -1,9 +1,10 @@
 package jom.com.softserve.s6.task1;
 
-import java.time.Year;
+import java.time.LocalDate;
 
 public class MyUtils {
     public static boolean isLeapYear(int year) {
-        return Year.isLeap(year);
+        LocalDate localDate = LocalDate.of(year, 1, 1);
+        return localDate.isLeapYear();
     }
 }
